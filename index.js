706e9a108e91e7a0fa91a690d52ffdb737dfdd2e
sopa <113952208+soufriaz@users.noreply.github.com>
2024-04-10 22:19:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const buttonMenu = document.querySelector("#btn-menu")
+
+function toggleMenu(){
+
+  const nav = document.querySelector(".nav")
+  nav.classList.toggle('active')
+
+}
+
+
+
+buttonMenu.addEventListener('click', toggleMenu)
